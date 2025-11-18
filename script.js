5f5ed6f31fda7e8c1babdad247a67a0503110e40
Oluwatobiloba Emmanuel Adejumo <oadejumo@xula.edu>
2025-11-18 17:07:07 -0600

    Updated main tag

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(".menu-tog").click(function() {
     const man = $("main");
     const ul = $("#sideBar");
-    if ((ul.css("display") === "none") | (man.css("height") !== "0px")) {
+    if ((ul.css("display") === "none") && (man.css("height") !== "0px")) {
         man.css("height", "0px");
         ul.css("display", "block");
     } else {
