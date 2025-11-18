49a0be50f506872951405bf6467ab386f2ee26b6
Chidush <tobiadejumo10@gmail.com>
2025-11-18 16:28:55 -0600

    Testing

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(".menu-tog").click(function() {
+    const man = $("main");
     const ul = $("#sideBar");
-    if ((ul.css("display") === "none") | (ul.css("height") !== "100vh")) {
-        ul.css("height", "100vh");
+    if ((ul.css("display") === "none") | (man.css("height") !== "calc(100vh - 74px)")) {
+        man.css("height", "calc(100vh - 74px)");
         ul.css("display", "block");
     } else {
         ul.css("display", "none");
-        ul.css("height", "auto");
+        man.css("height", "auto");
     }
 });
