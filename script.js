5d401b1ca60349ec8932b897abb1d291e037dc14
Oluwatobiloba Emmanuel Adejumo <oadejumo@xula.edu>
2025-11-18 16:54:21 -0600

    Testing

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(".menu-tog").click(function() {
     const man = $("main");
     const ul = $("#sideBar");
-    if ((ul.css("display") === "none") | (man.css("height") !== "calc(100vh - 74px)")) {
-        man.css("height", "calc(100vh - 74px)");
+    if ((ul.css("display") === "none") | (man.css("height") !== "0px")) {
+        man.css("height", "0px");
         ul.css("display", "block");
     } else {
         ul.css("display", "none");
